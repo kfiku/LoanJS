9e78cfa5b58a4e48c2b05e1f3e0db281f2fd49e8
Grzegorz Klimek <kfiku.com@gmail.com>
2014-11-08 11:37:47 +0100

    update test - more accurate

--- test/loanjs_test.js
+++ test/loanjs_test.js
@@ -1,15 +1,32 @@
 /*global describe,it*/
 'use strict';
 var assert  = require('assert'),
-    Loan    = require('../lib/loanjs.js');
+    Loan    = require('../lib/loan.js');
 
 describe('loanjs node module.', function() {
-  it('must be awesome', function() {
-    var loan = new Loan(100000, 20, 3.5, true);
+  it('must count loan correctly', function() {
+    var loan = new Loan(100000, 20, 3.5, true),
+        sum = 0,
+        interestSum = 0;
 
     assert(loan.installments.length === 20, true);
-    assert(loan.interestSum > 0, true);
-    assert(loan.capitalSum >= 100000, true);
-    assert(loan.sum === loan.interestSum + loan.capitalSum, true);
+    for (var i = 0; i < loan.installments.length; i++) {
+      var inst = loan.installments[i];
+      assert(inst.capital > 0, true);
+      assert(inst.intrest > 0, true);
+      assert(inst.installment === inst.capital + inst.intrest, true);
+
+      sum += inst.installment;
+      interestSum += inst.intrest;
+    }
+
+    assert(loan.interestSum > 0, true, 'wrong interestSum');
+    assert.equal(loan.capitalSum, 100000, 'wrong capitalSum');
+    assert.equal(loan.interestSum, interestSum,
+                'intrestsSum ('+loan.interestSum+') not exual to sum of all intrests ('+interestSum+') in instalments array');
+    assert.equal(loan.sum, loan.interestSum + loan.capitalSum, true,
+                'sum ('+loan.sum+') not equal to interestSum + capitalSum ('+(loan.interestSum + loan.capitalSum)+')');
+    assert.equal(loan.sum, sum, true,
+                'sum ('+loan.sum+') not exual to sum of all instalment ('+sum+') in instalments array');
   });
 });
