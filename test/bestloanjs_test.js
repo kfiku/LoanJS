8c982184ec27110f52c89dac2fa4d5562587b0fa
Grzegorz Klimek <kfiku.com@gmail.com>
2014-11-26 23:58:49 +0100

    fix loans shorter then 12 installments/months

--- test/bestloanjs_test.js
+++ test/bestloanjs_test.js
@@ -20,4 +20,21 @@ describe('loanjs node module.', function() {
     }
 
   });
+
+  it('must count best loan lower then 12m', function() {
+    // amount, maxInstallment, maxInstallmentsNumber, interestRate, savingsInterestRate, params
+    var variants = new BestLoan(20000, 3000, 12, 5, 3),
+        v,
+        b = variants.best;
+
+    assert.ok(b, 'shoult be best variant');
+    assert.ok(b.pointOfContact, 'shoult be best variant pointOfContact');
+    assert.ok(b.pointOfContact.instNr <= b.instNr, 'best variant pointOfContact instNr shoult be >= variants instNr');
+
+    for (var i = 0; i < variants.variants.length; i++) {
+      v = variants.variants[i];
+      assert.ok(b.pointOfContact.costs <= v.pointOfContact.costs, 'best variant cost shoult be lowest');
+    }
+
+  });
 });
