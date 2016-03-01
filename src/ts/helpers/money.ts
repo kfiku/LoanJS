9615e3b25321fc9a4c6a5ba53ae23cad908fe183
Grzegorz Klimek <kfiku.com@gmail.com>
2016-03-02 00:28:43 +0100

    formating money

---
+++ src/ts/helpers/money.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+let numeral = require('numeral');
+let format = {
+  pl: '0,0.00 $',
+  en: '$0,0.00'
+}
+
+numeral.language('pl', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    currency: {
+        symbol: 'zł'
+    }
+});
+
+// switch between languages
+numeral.language(window['lang']);
+
+let money = function (n) {
+    return numeral(n).format(format[window['lang']]);
+};
+
+export = money;
